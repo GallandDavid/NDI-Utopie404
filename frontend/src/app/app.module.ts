db1dba74ac96b0ee652b9af97afd8af5957c93bd
Galland David <galland.david33@gmail.com>
2023-12-07 23:30:07 +0100

    home et bouttons display

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ToolsComponent } from './home/tools/tools.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
